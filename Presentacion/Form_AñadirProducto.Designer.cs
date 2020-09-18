c3d7388e093ecaa4eed27963aa3e39a1b604e599
IvanLeuze <darkar564@gmail.com>
2020-09-18 17:29:40 -0300

    Juan.3

---
+++ Presentacion/Form_AñadirProducto.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ShockSoft.Presentacion
+{
+    partial class Form_AñadirProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblIVA = new System.Windows.Forms.Label();
+            this.lblGanancia = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.comboIVA = new System.Windows.Forms.ComboBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtGanancia = new System.Windows.Forms.TextBox();
+            this.comboMarca = new System.Windows.Forms.ComboBox();
+            this.btnAgregarMarca = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnAceptar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(80, 54);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 0;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(80, 102);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 1;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // lblIVA
+            // 
+            this.lblIVA.AutoSize = true;
+            this.lblIVA.Location = new System.Drawing.Point(80, 150);
+            this.lblIVA.Name = "lblIVA";
+            this.lblIVA.Size = new System.Drawing.Size(24, 13);
+            this.lblIVA.TabIndex = 2;
+            this.lblIVA.Text = "IVA";
+            // 
+            // lblGanancia
+            // 
+            this.lblGanancia.AutoSize = true;
+            this.lblGanancia.Location = new System.Drawing.Point(80, 201);
+            this.lblGanancia.Name = "lblGanancia";
+            this.lblGanancia.Size = new System.Drawing.Size(67, 13);
+            this.lblGanancia.TabIndex = 3;
+            this.lblGanancia.Text = "Ganancia(%)";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(80, 250);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(37, 13);
+            this.lblMarca.TabIndex = 4;
+            this.lblMarca.Text = "Marca";
+            // 
+            // comboIVA
+            // 
+            this.comboIVA.FormattingEnabled = true;
+            this.comboIVA.Location = new System.Drawing.Point(213, 147);
+            this.comboIVA.Name = "comboIVA";
+            this.comboIVA.Size = new System.Drawing.Size(195, 21);
+            this.comboIVA.TabIndex = 5;
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(213, 51);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(195, 20);
+            this.txtDescripcion.TabIndex = 6;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(213, 99);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(195, 20);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // txtGanancia
+            // 
+            this.txtGanancia.Location = new System.Drawing.Point(213, 198);
+            this.txtGanancia.Name = "txtGanancia";
+            this.txtGanancia.Size = new System.Drawing.Size(195, 20);
+            this.txtGanancia.TabIndex = 8;
+            // 
+            // comboMarca
+            // 
+            this.comboMarca.FormattingEnabled = true;
+            this.comboMarca.Location = new System.Drawing.Point(213, 247);
+            this.comboMarca.Name = "comboMarca";
+            this.comboMarca.Size = new System.Drawing.Size(195, 21);
+            this.comboMarca.TabIndex = 9;
+            // 
+            // btnAgregarMarca
+            // 
+            this.btnAgregarMarca.Location = new System.Drawing.Point(448, 247);
+            this.btnAgregarMarca.Name = "btnAgregarMarca";
+            this.btnAgregarMarca.Size = new System.Drawing.Size(101, 23);
+            this.btnAgregarMarca.TabIndex = 10;
+            this.btnAgregarMarca.Text = "Añadir Marca";
+            this.btnAgregarMarca.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(448, 307);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(101, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnAceptar
+            // 
+            this.btnAceptar.Location = new System.Drawing.Point(574, 307);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.Size = new System.Drawing.Size(101, 23);
+            this.btnAceptar.TabIndex = 12;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.UseVisualStyleBackColor = true;
+            // 
+            // Form_AñadirProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(687, 342);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnAgregarMarca);
+            this.Controls.Add(this.comboMarca);
+            this.Controls.Add(this.txtGanancia);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.comboIVA);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.lblGanancia);
+            this.Controls.Add(this.lblIVA);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblDescripcion);
+            this.Name = "Form_AñadirProducto";
+            this.Text = "Form_AñadirProducto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblIVA;
+        private System.Windows.Forms.Label lblGanancia;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.ComboBox comboIVA;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtGanancia;
+        private System.Windows.Forms.ComboBox comboMarca;
+        private System.Windows.Forms.Button btnAgregarMarca;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnAceptar;
+    }
+}
---
+++ Presentacion/Form_ConsultarProducto.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace ShockSoft.Presentacion
+{
+    partial class Form_ConsultarProducto
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblFiltro = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.cbMostrarProductosBaja = new System.Windows.Forms.CheckBox();
+            this.dgProductos = new System.Windows.Forms.DataGridView();
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnAltaBaja = new System.Windows.Forms.Button();
+            this.btnHistorialVenta = new System.Windows.Forms.Button();
+            this.btnHistorialCompra = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgProductos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Location = new System.Drawing.Point(13, 19);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(37, 13);
+            this.lblFiltro.TabIndex = 0;
+            this.lblFiltro.Text = "Filtros:";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(87, 19);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(66, 13);
+            this.lblDescripcion.TabIndex = 1;
+            this.lblDescripcion.Text = "Descripción:";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(185, 16);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(222, 20);
+            this.txtDescripcion.TabIndex = 2;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(454, 19);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(21, 13);
+            this.lblID.TabIndex = 3;
+            this.lblID.Text = "ID:";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(501, 16);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(222, 20);
+            this.txtID.TabIndex = 4;
+            // 
+            // cbMostrarProductosBaja
+            // 
+            this.cbMostrarProductosBaja.AutoSize = true;
+            this.cbMostrarProductosBaja.Location = new System.Drawing.Point(90, 55);
+            this.cbMostrarProductosBaja.Name = "cbMostrarProductosBaja";
+            this.cbMostrarProductosBaja.Size = new System.Drawing.Size(180, 17);
+            this.cbMostrarProductosBaja.TabIndex = 5;
+            this.cbMostrarProductosBaja.Text = "mostrar productos dados de baja";
+            this.cbMostrarProductosBaja.UseVisualStyleBackColor = true;
+            // 
+            // dgProductos
+            // 
+            this.dgProductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgProductos.Location = new System.Drawing.Point(12, 81);
+            this.dgProductos.Name = "dgProductos";
+            this.dgProductos.Size = new System.Drawing.Size(776, 272);
+            this.dgProductos.TabIndex = 6;
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.Location = new System.Drawing.Point(16, 415);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(75, 23);
+            this.btnAñadir.TabIndex = 7;
+            this.btnAñadir.Text = "Añadir";
+            this.btnAñadir.UseVisualStyleBackColor = true;
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(107, 415);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(75, 23);
+            this.btnModificar.TabIndex = 8;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            // 
+            // btnAltaBaja
+            // 
+            this.btnAltaBaja.Location = new System.Drawing.Point(204, 415);
+            this.btnAltaBaja.Name = "btnAltaBaja";
+            this.btnAltaBaja.Size = new System.Drawing.Size(75, 23);
+            this.btnAltaBaja.TabIndex = 9;
+            this.btnAltaBaja.Text = "Alta/Baja";
+            this.btnAltaBaja.UseVisualStyleBackColor = true;
+            // 
+            // btnHistorialVenta
+            // 
+            this.btnHistorialVenta.Location = new System.Drawing.Point(650, 415);
+            this.btnHistorialVenta.Name = "btnHistorialVenta";
+            this.btnHistorialVenta.Size = new System.Drawing.Size(138, 23);
+            this.btnHistorialVenta.TabIndex = 10;
+            this.btnHistorialVenta.Text = "Historial de Venta";
+            this.btnHistorialVenta.UseVisualStyleBackColor = true;
+            // 
+            // btnHistorialCompra
+            // 
+            this.btnHistorialCompra.Location = new System.Drawing.Point(501, 415);
+            this.btnHistorialCompra.Name = "btnHistorialCompra";
+            this.btnHistorialCompra.Size = new System.Drawing.Size(138, 23);
+            this.btnHistorialCompra.TabIndex = 11;
+            this.btnHistorialCompra.Text = "Historial de Compra";
+            this.btnHistorialCompra.UseVisualStyleBackColor = true;
+            // 
+            // Form_ConsultarProducto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHistorialCompra);
+            this.Controls.Add(this.btnHistorialVenta);
+            this.Controls.Add(this.btnAltaBaja);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnAñadir);
+            this.Controls.Add(this.dgProductos);
+            this.Controls.Add(this.cbMostrarProductosBaja);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblFiltro);
+            this.Name = "Form_ConsultarProducto";
+            this.Text = "Form_ConsultarProducto";
+            ((System.ComponentModel.ISupportInitialize)(this.dgProductos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblFiltro;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.CheckBox cbMostrarProductosBaja;
+        private System.Windows.Forms.DataGridView dgProductos;
+        private System.Windows.Forms.Button btnAñadir;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnAltaBaja;
+        private System.Windows.Forms.Button btnHistorialVenta;
+        private System.Windows.Forms.Button btnHistorialCompra;
+    }
+}
