2a10b29c15d664ca3cc127732f3e16ceb696707b
IvanLeuze <darkar564@gmail.com>
2020-11-13 15:59:05 -0300

    JuanVisuals3.8

--- Presentacion/Form_AgregarVenta.cs
+++ Presentacion/Form_AgregarVenta.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using ShockSoft.Dominio;
+using System.Runtime.InteropServices;
 
 namespace ShockSoft.Presentacion
 {
@@ -35,11 +36,6 @@ namespace ShockSoft.Presentacion
             this.Show();
         }
 
-        private void BtnAceptar_Click(object sender, EventArgs e)
-        {
-            controlador.AgregarVenta(int.Parse(txtId.Text), controlador.GenerarLineasDeVenta(dglineasDeVenta.Rows), (int)comboMetodoPago.SelectedValue);
-            MessageBox.Show("La venta se ha registrado exitosamente", "Éxito");
-        }
 
         public void AgregarLineaDeVenta(string pIdProducto, string pDescripcion, string pPrecioActual, int pCantidad)
         {
@@ -50,6 +46,46 @@ namespace ShockSoft.Presentacion
                 total += (double)dglineasDeVenta.Rows[i].Cells[4].Value;
             }
             txtTotal.Text = total.ToString();
+        }
+
+        // Deslizar ventana desde el panel de control
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int IParam);
+
+        private void panelControl_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btnTamano_Click(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Maximized)
+            {
+                this.WindowState = FormWindowState.Normal;
+            }
+            else
+            {
+                this.WindowState = FormWindowState.Maximized;
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            controlador.AgregarVenta(int.Parse(txtId.Text), controlador.GenerarLineasDeVenta(dglineasDeVenta.Rows), (int)comboMetodoPago.SelectedValue);
+            MessageBox.Show("La venta se ha registrado exitosamente", "Éxito");
         }
     }
 }
