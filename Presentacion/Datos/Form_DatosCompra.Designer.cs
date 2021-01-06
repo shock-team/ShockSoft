d55d4941b90c91929a1be487c9ec7f3be91692b5
IvanLeuze <darkar564@gmail.com>
2021-01-06 17:20:48 -0300

    JuanVisuals4.7

--- Presentacion/Datos/Form_DatosCompra.Designer.cs
+++ Presentacion/Datos/Form_DatosCompra.Designer.cs
@@ -30,6 +30,11 @@
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.comboProveedores = new System.Windows.Forms.ComboBox();
             this.lblProveedor = new System.Windows.Forms.Label();
             this.dgLineasDeCompra = new System.Windows.Forms.DataGridView();
@@ -44,24 +49,34 @@
             this.lblFecha = new System.Windows.Forms.Label();
             this.lblIdCompra = new System.Windows.Forms.Label();
             this.txtIdCompra = new System.Windows.Forms.TextBox();
+            this.panelControl = new System.Windows.Forms.Panel();
+            this.btnMinimizar = new System.Windows.Forms.Button();
+            this.picShock = new System.Windows.Forms.PictureBox();
+            this.lblShock = new System.Windows.Forms.Label();
+            this.btnTamano = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgLineasDeCompra)).BeginInit();
+            this.panelControl.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picShock)).BeginInit();
             this.SuspendLayout();
             // 
             // comboProveedores
             // 
             this.comboProveedores.Enabled = false;
+            this.comboProveedores.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
             this.comboProveedores.FormattingEnabled = true;
-            this.comboProveedores.Location = new System.Drawing.Point(276, 26);
+            this.comboProveedores.Location = new System.Drawing.Point(377, 57);
             this.comboProveedores.Name = "comboProveedores";
-            this.comboProveedores.Size = new System.Drawing.Size(220, 21);
+            this.comboProveedores.Size = new System.Drawing.Size(392, 27);
             this.comboProveedores.TabIndex = 13;
             // 
             // lblProveedor
             // 
             this.lblProveedor.AutoSize = true;
-            this.lblProveedor.Location = new System.Drawing.Point(211, 31);
+            this.lblProveedor.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lblProveedor.Location = new System.Drawing.Point(273, 60);
             this.lblProveedor.Name = "lblProveedor";
-            this.lblProveedor.Size = new System.Drawing.Size(59, 13);
+            this.lblProveedor.Size = new System.Drawing.Size(85, 19);
             this.lblProveedor.TabIndex = 12;
             this.lblProveedor.Text = "Proveedor:";
             // 
@@ -69,6 +84,16 @@
             // 
             this.dgLineasDeCompra.AllowUserToAddRows = false;
             this.dgLineasDeCompra.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dgLineasDeCompra.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgLineasDeCompra.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
             this.dgLineasDeCompra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgLineasDeCompra.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.ID,
@@ -76,9 +101,27 @@
             this.PrecioUnitario,
             this.Cantidad,
             this.Subtotal});
-            this.dgLineasDeCompra.Location = new System.Drawing.Point(27, 53);
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle5.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle5.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle5.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle5.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle5.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgLineasDeCompra.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dgLineasDeCompra.Location = new System.Drawing.Point(28, 145);
             this.dgLineasDeCompra.Name = "dgLineasDeCompra";
             this.dgLineasDeCompra.ReadOnly = true;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgLineasDeCompra.RowHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dgLineasDeCompra.RowsDefaultCellStyle = dataGridViewCellStyle7;
             this.dgLineasDeCompra.Size = new System.Drawing.Size(743, 246);
             this.dgLineasDeCompra.TabIndex = 18;
             // 
@@ -97,9 +140,9 @@
             // 
             // PrecioUnitario
             // 
-            dataGridViewCellStyle1.Format = "C2";
-            dataGridViewCellStyle1.NullValue = null;
-            this.PrecioUnitario.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle3.Format = "C2";
+            dataGridViewCellStyle3.NullValue = null;
+            this.PrecioUnitario.DefaultCellStyle = dataGridViewCellStyle3;
             this.PrecioUnitario.HeaderText = "Precio unitario";
             this.PrecioUnitario.Name = "PrecioUnitario";
             this.PrecioUnitario.ReadOnly = true;
@@ -112,9 +155,9 @@
             // 
             // Subtotal
             // 
-            dataGridViewCellStyle2.Format = "C2";
-            dataGridViewCellStyle2.NullValue = null;
-            this.Subtotal.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridViewCellStyle4.Format = "C2";
+            dataGridViewCellStyle4.NullValue = null;
+            this.Subtotal.DefaultCellStyle = dataGridViewCellStyle4;
             this.Subtotal.HeaderText = "Subtotal";
             this.Subtotal.Name = "Subtotal";
             this.Subtotal.ReadOnly = true;
@@ -122,60 +165,149 @@
             // txtTotal
             // 
             this.txtTotal.Enabled = false;
-            this.txtTotal.Location = new System.Drawing.Point(589, 305);
+            this.txtTotal.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.txtTotal.Location = new System.Drawing.Point(590, 406);
             this.txtTotal.Name = "txtTotal";
             this.txtTotal.ReadOnly = true;
-            this.txtTotal.Size = new System.Drawing.Size(181, 20);
+            this.txtTotal.Size = new System.Drawing.Size(181, 25);
             this.txtTotal.TabIndex = 17;
             // 
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(549, 309);
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lblTotal.Location = new System.Drawing.Point(537, 409);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(34, 13);
+            this.lblTotal.Size = new System.Drawing.Size(47, 19);
             this.lblTotal.TabIndex = 16;
             this.lblTotal.Text = "Total:";
             // 
             // dtpFecha
             // 
             this.dtpFecha.Enabled = false;
-            this.dtpFecha.Location = new System.Drawing.Point(568, 27);
+            this.dtpFecha.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.dtpFecha.Location = new System.Drawing.Point(113, 97);
             this.dtpFecha.Name = "dtpFecha";
-            this.dtpFecha.Size = new System.Drawing.Size(200, 20);
+            this.dtpFecha.Size = new System.Drawing.Size(273, 25);
             this.dtpFecha.TabIndex = 15;
             // 
             // lblFecha
             // 
             this.lblFecha.AutoSize = true;
-            this.lblFecha.Location = new System.Drawing.Point(522, 30);
+            this.lblFecha.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lblFecha.Location = new System.Drawing.Point(30, 102);
             this.lblFecha.Name = "lblFecha";
-            this.lblFecha.Size = new System.Drawing.Size(40, 13);
+            this.lblFecha.Size = new System.Drawing.Size(51, 19);
             this.lblFecha.TabIndex = 14;
             this.lblFecha.Text = "Fecha:";
             // 
             // lblIdCompra
             // 
             this.lblIdCompra.AutoSize = true;
-            this.lblIdCompra.Location = new System.Drawing.Point(24, 29);
+            this.lblIdCompra.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.lblIdCompra.Location = new System.Drawing.Point(30, 60);
             this.lblIdCompra.Name = "lblIdCompra";
-            this.lblIdCompra.Size = new System.Drawing.Size(21, 13);
+            this.lblIdCompra.Size = new System.Drawing.Size(27, 19);
             this.lblIdCompra.TabIndex = 19;
             this.lblIdCompra.Text = "ID:";
             // 
             // txtIdCompra
             // 
-            this.txtIdCompra.Location = new System.Drawing.Point(51, 26);
+            this.txtIdCompra.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Bold);
+            this.txtIdCompra.Location = new System.Drawing.Point(73, 57);
             this.txtIdCompra.Name = "txtIdCompra";
             this.txtIdCompra.ReadOnly = true;
-            this.txtIdCompra.Size = new System.Drawing.Size(118, 20);
+            this.txtIdCompra.Size = new System.Drawing.Size(163, 25);
             this.txtIdCompra.TabIndex = 20;
+            // 
+            // panelControl
+            // 
+            this.panelControl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.panelControl.Controls.Add(this.btnMinimizar);
+            this.panelControl.Controls.Add(this.picShock);
+            this.panelControl.Controls.Add(this.lblShock);
+            this.panelControl.Controls.Add(this.btnTamano);
+            this.panelControl.Controls.Add(this.btnCerrar);
+            this.panelControl.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl.Location = new System.Drawing.Point(0, 0);
+            this.panelControl.Name = "panelControl";
+            this.panelControl.Size = new System.Drawing.Size(800, 45);
+            this.panelControl.TabIndex = 32;
+            this.panelControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panelControl_MouseDown);
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinimizar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.btnMinimizar.FlatAppearance.BorderSize = 0;
+            this.btnMinimizar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.btnMinimizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMinimizar.Image = global::ShockSoft.Properties.Resources.Shock_img02;
+            this.btnMinimizar.Location = new System.Drawing.Point(665, 0);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(45, 45);
+            this.btnMinimizar.TabIndex = 10;
+            this.btnMinimizar.UseVisualStyleBackColor = false;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // picShock
+            // 
+            this.picShock.Image = global::ShockSoft.Properties.Resources.LOGO_ZONA;
+            this.picShock.Location = new System.Drawing.Point(3, 3);
+            this.picShock.Name = "picShock";
+            this.picShock.Size = new System.Drawing.Size(40, 40);
+            this.picShock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picShock.TabIndex = 9;
+            this.picShock.TabStop = false;
+            // 
+            // lblShock
+            // 
+            this.lblShock.AutoSize = true;
+            this.lblShock.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblShock.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblShock.Location = new System.Drawing.Point(40, 9);
+            this.lblShock.Name = "lblShock";
+            this.lblShock.Size = new System.Drawing.Size(118, 28);
+            this.lblShock.TabIndex = 8;
+            this.lblShock.Text = "Zona!Soft";
+            // 
+            // btnTamano
+            // 
+            this.btnTamano.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTamano.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.btnTamano.FlatAppearance.BorderSize = 0;
+            this.btnTamano.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(100)))), ((int)(((byte)(100)))));
+            this.btnTamano.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTamano.Image = global::ShockSoft.Properties.Resources.Shock_img03;
+            this.btnTamano.Location = new System.Drawing.Point(710, 0);
+            this.btnTamano.Name = "btnTamano";
+            this.btnTamano.Size = new System.Drawing.Size(45, 45);
+            this.btnTamano.TabIndex = 7;
+            this.btnTamano.UseVisualStyleBackColor = false;
+            this.btnTamano.Click += new System.EventHandler(this.btnTamano_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCerrar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(45)))));
+            this.btnCerrar.FlatAppearance.BorderSize = 0;
+            this.btnCerrar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(232)))), ((int)(((byte)(17)))), ((int)(((byte)(35)))));
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCerrar.Image = global::ShockSoft.Properties.Resources.Shock_img04;
+            this.btnCerrar.Location = new System.Drawing.Point(755, 0);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(45, 45);
+            this.btnCerrar.TabIndex = 6;
+            this.btnCerrar.UseVisualStyleBackColor = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
             // 
             // Form_DatosCompra
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 344);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(80)))), ((int)(((byte)(75)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panelControl);
             this.Controls.Add(this.txtIdCompra);
             this.Controls.Add(this.lblIdCompra);
             this.Controls.Add(this.dgLineasDeCompra);
@@ -185,9 +317,13 @@
             this.Controls.Add(this.lblFecha);
             this.Controls.Add(this.comboProveedores);
             this.Controls.Add(this.lblProveedor);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form_DatosCompra";
             this.Text = "Datos Compra";
             ((System.ComponentModel.ISupportInitialize)(this.dgLineasDeCompra)).EndInit();
+            this.panelControl.ResumeLayout(false);
+            this.panelControl.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picShock)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -209,5 +345,11 @@
         private System.Windows.Forms.DataGridViewTextBoxColumn PrecioUnitario;
         private System.Windows.Forms.DataGridViewTextBoxColumn Cantidad;
         private System.Windows.Forms.DataGridViewTextBoxColumn Subtotal;
+        private System.Windows.Forms.Panel panelControl;
+        private System.Windows.Forms.Button btnMinimizar;
+        private System.Windows.Forms.PictureBox picShock;
+        private System.Windows.Forms.Label lblShock;
+        private System.Windows.Forms.Button btnTamano;
+        private System.Windows.Forms.Button btnCerrar;
     }
 }
